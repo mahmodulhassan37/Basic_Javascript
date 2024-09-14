4ce83149ef13414e48611308ad43b0cc414de8ac
MD.Mahmodul Hassan <55808463+mahmodulhassan37@users.noreply.github.com>
2024-09-14 09:42:03 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,18 @@
+//object insert into the Array
+const products = [
+    {name:"mobile",price:24000, origin:"Usa"},
+    {name:"Laptop",price:32000,origin:"Germany"},
+]
+//console.log(products[0].origin);
+
+//array inert into the object
+
+const price = {
+    '0': 15,
+    '1': 25,
+    '2': 43
+}
+
+console.log(price[2])
+
+
